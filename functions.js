ca4c32f569297c21f6190bba6da2dcb96ff33f36
mara-olson <mara.c.olson@gmail.com>
2022-03-24 21:01:21 -0700

    Translated calculateTotal function to JS

--- functions.js
+++ functions.js
@@ -14,4 +14,19 @@ function getFullName(firstName, lastName) {
 
 // 3. calculateTotal
 
-funn;
+function calculateTotal(base_price, state, tax = 0.05) {
+  const subtotal = base_price * (1 + tax);
+  let fee = 0;
+  if (state === "CA") {
+    fee = 0.03 * subtotal;
+  } else if (state === "PA") {
+    fee = 2;
+  } else if (state === "MA") {
+    if (base_price <= 100) {
+      fee = 1;
+    } else {
+      fee = 3;
+    }
+  }
+  return subtotal + fee;
+}
