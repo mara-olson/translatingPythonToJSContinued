de3509e91df047d1816a2aed6dd0db39ba6510ca
mara-olson <mara.c.olson@gmail.com>
2022-03-24 20:47:12 -0700

    Translated isHometown and getFullName to JS

--- functions.js
+++ functions.js
@@ -1,13 +1,17 @@
-'use strict';
+"use strict";
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town) {
+  return town === "San Francisco";
+}
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(firstName, lastName) {
+  console.log(`${firstName} ${lastName}`);
+}
 
 // 3. calculateTotal
 
-// Define your function here
+funn;
